10665744460a5d70094a959b46dae4a5b8b62383
a23davidcg <a23davidcg@iessanclemente.net>
2023-10-13 13:11:06 +0200

    ....

--- ud2/UD_2_4_LinearLayoutConVariosDentro/app/src/main/java/com/example/ud_2_4_linearlayoutconvariosdentro/MainActivity.kt
+++ ud2/UD_2_4_LinearLayoutConVariosDentro/app/src/main/java/com/example/ud_2_4_linearlayoutconvariosdentro/MainActivity.kt
@@ -4,21 +4,29 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val llamar  =  findViewById<Button>(R.id.button)
-        val llamada  =  findViewById<TextView>(R.id.textView3)
-        val juan =  findViewById<TextView>(R.id.text1)
+        val botonLlamada  = findViewById<Button>(R.id.ButtonLlamar) /*Se asigna a una variable local el valor de una variable a traves
+        de la id del boton del xml que se ha creado con anterioridad */
+        val llamando = findViewById<TextView>(R.id.textView2)
+        val nombreLlamada = findViewById<TextView>(R.id.Juan)
+        val texto = nombreLlamada.text.toString()
+        botonLlamada.setOnClickListener(){
+            //Una vez se presiona el boton se eejcuta el siguiente código
+            val mensaje = "Se está llamando a  " + texto
+
+            llamando.text = mensaje
 
 
-        llamar.setOnClickListener {
-            llamada.text = "Llamando a " //+ juan
+
         }
 
 
     }
+
 }
