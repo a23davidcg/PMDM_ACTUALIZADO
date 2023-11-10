36db581d946d1bddbf765233febc3d78bd18cf32
a23davidcg <a23davidcg@iessanclemente.net>
2023-11-10 12:47:34 +0100

    ..

--- UD_05_Pizza/UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
+++ UD_05_Pizza/UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass.
@@ -14,7 +18,6 @@ import com.google.android.material.appbar.MaterialToolbar
  * create an instance of this fragment.
  */
 class OrderFragment : Fragment() {
-
 
 
     override fun onCreateView(
@@ -25,7 +28,27 @@ class OrderFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fabNext = view.findViewById<FloatingActionButton>(R.id.fab)
+        fabNext.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.group_pizza_type)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            var msg = ""
+            if (pizzaType == -1) {
+                msg = "Debes seleccionar un tipo de pizza"
+                Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
+            } else {
+                msg = when (pizzaType) {
+                    R.id.radio_margarita -> "Margarita"
+                    R.id.radio_calzone -> "Calzone"
+                    else -> "No existe"
+
+                }
+                //Extras
+                var parmesano = view.findViewById<Chip>(R.id.chip_parmesano)
+                if (parmesano.isChecked) msg+= "con parmesano"
+            }
+        }
         return view
     }
-
 }
