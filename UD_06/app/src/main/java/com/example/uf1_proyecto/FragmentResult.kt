8b7b098256b5b0e5c6881d36bba9c2541f2b3b95
a23davidcg <a23davidcg@iessanclemente.net>
2023-11-17 14:26:52 +0100

    ..

--- UD_06/app/src/main/java/com/example/uf1_proyecto/FragmentResult.kt
+++ UD_06/app/src/main/java/com/example/uf1_proyecto/FragmentResult.kt
@@ -7,37 +7,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
-import com.example.uf1_proyecto.databinding.FragmentGameBinding
+import androidx.navigation.findNavController
+import com.example.uf1_proyecto.databinding.FragmentResultBinding
 
 
 class FragmentResult : Fragment() {
 
-    private var _binding : FragmentGameBinding? = null
-
+    private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_game, container, false)
-        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
-        val gameModel : GameViewModel by viewModels(
-            ownerProducer = { this.requireActivity()}
+        val gameModel: GameViewModel by viewModels(
+            ownerProducer = { this.requireActivity() }
         )
-        
-        binding.buttonNext.setOnClickListener{
 
-            Toast.makeText(activity, gameModel.secretWord, Toast.LENGTH_LONG)
+        binding.resultbutton.setOnClickListener {
+            gameModel.restart()
+            view.findNavController().navigate(R.id.action_fragmentResult_to_gameFragment)
+            Toast.makeText(activity, gameModel.secretDisplay, Toast.LENGTH_LONG)
         }
         return view
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
-
 }
--- UD_06/app/src/main/java/com/example/uf1_proyecto/GameFragment.kt
+++ UD_06/app/src/main/java/com/example/uf1_proyecto/GameFragment.kt
@@ -1,35 +1,47 @@
 package com.example.uf1_proyecto
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.uf1_proyecto.databinding.FragmentGameBinding
 
-
 class GameFragment : Fragment() {
 
     private var _binding: FragmentGameBinding? = null
-
     private val binding get() = _binding!!
+    val gameModel: GameViewModel by viewModels(ownerProducer = { this.requireActivity() })
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_game, container, false)
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val view = binding.root
-        val gameModel: GameViewModel by viewModels()
-        binding.buttonNext.setOnClickListener {
-            gameModel.secretWord = "Palabra de prueba"
-            view.findNavController().navigate(R.id.action_gameFragment_to_fragmentResult)
-        }
 
+
+        binding.buttonNext.setOnClickListener {
+            val userInput = binding.txtGuess.text.toString()
+            if (userInput.isNotEmpty()) {
+                gameModel.game(userInput[0])
+                updateScreen()
+                if (gameModel.win() || gameModel.lose())
+                    view.findNavController().navigate(R.id.action_gameFragment_to_fragmentResult)
+            } else {
+                Toast.makeText(context, "Introduce una letra", Toast.LENGTH_SHORT).show()
+            }
+        }
+        gameModel.secretWordDisplay.observe(
+            viewLifecycleOwner, Observer {
+                it -> binding.txtWord = it
+
+            }
+        )
         return view
+
     }
 
     override fun onDestroy() {
@@ -37,5 +49,8 @@ class GameFragment : Fragment() {
         _binding = null
     }
 
-
+    fun updateScreen() {
+        binding.txtWord.text = gameModel.secretDisplay
+        binding.txtLives.text = "te quedan ${gameModel.lives} vidas"
+    }
 }
