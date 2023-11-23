192295ac35e990f76c73881e3815af71ecb99261
a23davidcg <a23davidcg@iessanclemente.net>
2023-11-23 23:20:45 +0100

    ..

--- Proyecto/app/build.gradle.kts
+++ Proyecto/app/build.gradle.kts
@@ -70,6 +70,9 @@ dependencies {
 
     implementation("com.google.firebase:firebase-analytics")
 
+//para la navegacion
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation( "androidx.navigation:navigation-ui-ktx:2.7.5")
 
 
 
--- Proyecto/app/src/main/java/com/example/proyecto/LoginFragment.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/LoginFragment.kt
@@ -1,23 +1,20 @@
 package com.example.proyecto
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.firebase.analytics.FirebaseAnalytics
 
 
 class LoginFragment : Fragment() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        //Logica de a pantalla
-//        setup()
-//    }
-//}
-//
-//private fun setup() {
-//    title = "Autenticación"
-//
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
 
 
-
+    }
 }
