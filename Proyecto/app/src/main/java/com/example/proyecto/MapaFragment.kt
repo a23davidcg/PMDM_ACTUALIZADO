a23f2589cd2afd25c8155fe3ce81ee89a9caf564
a23davidcg <a23davidcg@iessanclemente.net>
2023-12-02 16:58:50 +0100

    ....

--- Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
@@ -1,76 +1,19 @@
-//import android.os.Bundle
-//import android.util.Log
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import androidx.fragment.app.Fragment
-//import com.google.android.gms.maps.CameraUpdateFactory
-//import com.google.android.gms.maps.GoogleMap
-//import com.google.android.gms.maps.OnMapReadyCallback
-//import com.google.android.gms.maps.SupportMapFragment
-//import com.google.android.gms.maps.model.LatLng
-//import com.google.android.gms.maps.model.MarkerOptions
-//import com.example.proyecto.R
-//
-// class MapaFragment : Fragment(), OnMapReadyCallback {
-//
-//    private lateinit var googleMap: GoogleMap
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflar el diseño del fragmento
-//        return inflater.inflate(R.layout.fragment_mapa, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        val mapFragment = childFragmentManager.findFragmentById(R.id.map)
-//
-//        if (mapFragment is SupportMapFragment){
-//            mapFragment.getMapAsync(this)
-//        }else{
-//            Log.e("MapaFragment", "El fragmento no es de tipo SupportMapFragment)
-//        }
-//
-//        // Obtener una referencia al mapa desde el fragmento
-////        val mapFragment =
-////            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-////        mapFragment?.getMapAsync(this)
-//    }
-//
-//    override fun onMapReady(map: GoogleMap?) {
-//        map?.let {
-//            googleMap = it
-//
-//            try {
-//                // Configurar el mapa según sea necesario
-//                val location = LatLng(0.0, 0.0) // Coordenadas de ejemplo
-//                googleMap.addMarker(MarkerOptions().position(location).title("Marcador de ejemplo"))
-//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//            }
-//        }
-//    }
-//}
-
-
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.proyecto.R
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.example.proyecto.R
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapaFragment : Fragment() {
 
     private lateinit var googleMap: GoogleMap
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -84,50 +27,25 @@ class MapaFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Initialize the fragment of the map
-        val mapFragment = childFragmentManager.findFragmentById(R.id.mapaFragment) as SupportMapFragment
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapaFragment) as SupportMapFragment
         mapFragment.getMapAsync { map ->
             // Configure the GoogleMap
             googleMap = map
+            configurarMapa()
             // You can perform additional map configurations here
         }
     }
+
+    private fun configurarMapa() {
+        // Configuración adicional del mapa
+        // Ejemplo: Mover la cámara a una ubicación específica y agregar un marcador
+
+        // Mover la cámara a una ubicación específica (por ejemplo, latitud y longitud de un lugar)
+        val ubicacion = LatLng(37.7749, -122.4194) // Latitud y longitud de San Francisco, CA
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 12f)) // Zoom de 12
+
+        // Agregar un marcador en la ubicación
+        googleMap.addMarker(MarkerOptions().position(ubicacion).title("San Francisco"))
+    }
 }
-//
-//
-////import android.os.Bundle
-////import androidx.fragment.app.Fragment
-////import android.view.LayoutInflater
-////import android.view.View
-////import android.view.ViewGroup
-////import com.google.android.gms.maps.GoogleMap
-////import com.google.android.gms.maps.SupportMapFragment
-////import com.example.proyecto.R
-////
-////class MapaFragment : Fragment() {
-////
-////
-////
-////    private lateinit var googleMap: GoogleMap
-////
-////
-////
-////    override fun onCreateView(
-////        inflater: LayoutInflater, container: ViewGroup?,
-////        savedInstanceState: Bundle?
-////    ): View? {
-////        // Inflate the layout for this fragment
-////        return inflater.inflate(R.layout.fragment_mapa, container, false)
-////    }
-/////*
-////    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-////        super.onViewCreated(view, savedInstanceState)
-////
-////        // Inicializar el fragmento del mapa
-////        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-////        mapFragment.getMapAsync { map ->
-////            // Configurar el GoogleMap
-////            googleMap = map
-////            // Puedes realizar configuraciones adicionales del mapa aquí
-////        }
-////    }*/
-////}
