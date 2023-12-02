7c55fa243bbd5d12c7da9bb68d8bb289eb4fbe36
a23davidcg <a23davidcg@iessanclemente.net>
2023-12-02 02:07:25 +0100

    revisar layouts al completo

--- Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
@@ -1,3 +1,64 @@
+//import android.os.Bundle
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import com.google.android.gms.maps.CameraUpdateFactory
+//import com.google.android.gms.maps.GoogleMap
+//import com.google.android.gms.maps.OnMapReadyCallback
+//import com.google.android.gms.maps.SupportMapFragment
+//import com.google.android.gms.maps.model.LatLng
+//import com.google.android.gms.maps.model.MarkerOptions
+//import com.example.proyecto.R
+//
+// class MapaFragment : Fragment(), OnMapReadyCallback {
+//
+//    private lateinit var googleMap: GoogleMap
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflar el diseño del fragmento
+//        return inflater.inflate(R.layout.fragment_mapa, container, false)
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map)
+//
+//        if (mapFragment is SupportMapFragment){
+//            mapFragment.getMapAsync(this)
+//        }else{
+//            Log.e("MapaFragment", "El fragmento no es de tipo SupportMapFragment)
+//        }
+//
+//        // Obtener una referencia al mapa desde el fragmento
+////        val mapFragment =
+////            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+////        mapFragment?.getMapAsync(this)
+//    }
+//
+//    override fun onMapReady(map: GoogleMap?) {
+//        map?.let {
+//            googleMap = it
+//
+//            try {
+//                // Configurar el mapa según sea necesario
+//                val location = LatLng(0.0, 0.0) // Coordenadas de ejemplo
+//                googleMap.addMarker(MarkerOptions().position(location).title("Marcador de ejemplo"))
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+//    }
+//}
+
+
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,11 +70,7 @@ import com.example.proyecto.R
 
 class MapaFragment : Fragment() {
 
-
-
     private lateinit var googleMap: GoogleMap
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,16 +79,55 @@ class MapaFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mapa, container, false)
     }
-/*
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Inicializar el fragmento del mapa
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        // Initialize the fragment of the map
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapaFragment) as SupportMapFragment
         mapFragment.getMapAsync { map ->
-            // Configurar el GoogleMap
+            // Configure the GoogleMap
             googleMap = map
-            // Puedes realizar configuraciones adicionales del mapa aquí
+            // You can perform additional map configurations here
         }
-    }*/
+    }
 }
+//
+//
+////import android.os.Bundle
+////import androidx.fragment.app.Fragment
+////import android.view.LayoutInflater
+////import android.view.View
+////import android.view.ViewGroup
+////import com.google.android.gms.maps.GoogleMap
+////import com.google.android.gms.maps.SupportMapFragment
+////import com.example.proyecto.R
+////
+////class MapaFragment : Fragment() {
+////
+////
+////
+////    private lateinit var googleMap: GoogleMap
+////
+////
+////
+////    override fun onCreateView(
+////        inflater: LayoutInflater, container: ViewGroup?,
+////        savedInstanceState: Bundle?
+////    ): View? {
+////        // Inflate the layout for this fragment
+////        return inflater.inflate(R.layout.fragment_mapa, container, false)
+////    }
+/////*
+////    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+////        super.onViewCreated(view, savedInstanceState)
+////
+////        // Inicializar el fragmento del mapa
+////        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+////        mapFragment.getMapAsync { map ->
+////            // Configurar el GoogleMap
+////            googleMap = map
+////            // Puedes realizar configuraciones adicionales del mapa aquí
+////        }
+////    }*/
+////}
--- Proyecto/app/src/main/java/com/example/proyecto/MenuInicioFragment.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MenuInicioFragment.kt
@@ -13,7 +13,8 @@ import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class MenuInicioFragment : Fragment() {
 
@@ -25,61 +26,42 @@ class MenuInicioFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_menu_inicio, container, false)
         val drawerLayout: DrawerLayout = view.findViewById(R.id.drawer_layout)
         val navView: NavigationView = view.findViewById(R.id.navigation_view)
-        val toolbar: NavigationView = view.findViewById(R.id.toolbar)
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
         val bottomNavigationView: BottomNavigationView = view.findViewById(R.id.bottom_navigation)
 
 
-
-        //PAra el menu lateral emergente deslizando desde la izquierda debemos de:
-
-        //Debemos de inflar el menu lateral
-         // Inflar el menú lateral
+        // For the drawer layout
         val navController = findNavController()
 
-        var builderApp = AppBarConfiguration.Builder(navController.graph)
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
         builderApp.setOpenableLayout(drawerLayout)
-      //  navView.setupWithNavController(navController)
-
-
-
-
-        //FIXME: PANEL LATERAL- Referencia drawer layout
-
         val appBarConfiguration = builderApp.build()
-        setupActionBarWithNavController(navController = navController, appBarConfiguration)
 
+        // Setup the toolbar with NavController and AppBarConfiguration
+        toolbar.setupWithNavController(navController, appBarConfiguration)
 
+        // Setup the NavigationView with NavController
+        navView.setupWithNavController(navController)
+
+        // Navigation item selection listener for NavigationView
         navView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.Actividades -> {
-                    findNavController().navigate(R.id.action_menuInicio_to_activitiesFragment)
-                    // Acción para la opción 1
-                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
-                    return@setNavigationItemSelectedListener true
+                    navController.navigate(R.id.action_menuInicio_to_activitiesFragment)
+                    true
                 }
-
                 R.id.Encontrar -> {
-                    findNavController().navigate(R.id.action_menuInicio_to_mapaFragment)
-
-                    // Acción para la opción 2
-                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
-                    // replaceFragment(Fragment2())
-                    return@setNavigationItemSelectedListener true
+                     navController.navigate(R.id.action_menuInicio_to_mapaFragment)
+                    true
                 }
-
                 R.id.AmigosRecientes -> {
-                    findNavController().navigate(R.id.action_menuInicio_to_listaAmigosActivity)
-                    // Acción para la opción 2
-                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
-                    // replaceFragment(Fragment2())
-                    return@setNavigationItemSelectedListener true
-
+                    navController.navigate(R.id.action_menuInicio_to_listaAmigosFragment)
+                    true
                 }
                 R.id.AniadirAmigos -> {
-                    findNavController().navigate(R.id.action_menuInicio_to_agregarAmigosActivity)
-                    return@setNavigationItemSelectedListener true
+                    navController.navigate(R.id.action_menuInicio_to_aniadirAmigosFragment)
+                    true
                 }
-
                 else -> {
                     drawerLayout.closeDrawer(GravityCompat.START)
                     true
@@ -87,28 +69,169 @@ class MenuInicioFragment : Fragment() {
             }
         }
 
-
+        // Navigation item selection listener for BottomNavigationView
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.Ajustes -> {
-                    // Acción para la opción 1 del BottomNavigationView
-                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
-                    // replaceFragment(Fragment1())
+                    // Perform the action for Ajustes
                     true
                 }
-
                 R.id.info -> {
-                    // Acción para la opción 2 del BottomNavigationView
-                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
-                    //replaceFragment(Fragment2())
+                    // Perform the action for info
                     true
                 }
-
                 else -> false
             }
         }
 
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            menuItem ->
+            when (menuItem.itemId) {
+                R.id.nuevoLugar -> {
+                    //Configurar lo que se desee hacer a la hora de presionar el icono que hacemos referencia
+                    navController.navigate(R.id.action_menuInicio_to_mapaFragment)
+                    true
+
+                }
+                R.id.amigosBottomBar-> {
+                    //Confugrar lo que se vaya a queerer hacer con el icono de la parte inferiore del fragment
+                    navController.navigate(R.id.action_menuInicio_to_listaAmigosFragment)
+                    true
+                }
+                R.id.encontrarNuevo -> {
+
+                    //Configurar lo que se queira hacer al presionar el icono de la parte inferior de fragment
+
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+
+
         return view
     }
+
 }
 
+
+
+
+//package com.example.proyecto
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.core.view.GravityCompat
+//import androidx.drawerlayout.widget.DrawerLayout
+//import androidx.fragment.app.Fragment
+//import androidx.navigation.fragment.findNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+//import androidx.navigation.ui.setupWithNavController
+//import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.google.android.material.navigation.NavigationView
+//
+//
+//class MenuInicioFragment : Fragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        val view = inflater.inflate(R.layout.fragment_menu_inicio, container, false)
+//        val drawerLayout: DrawerLayout = view.findViewById(R.id.drawer_layout)
+//        val navView: NavigationView = view.findViewById(R.id.navigation_view)
+//        val toolbar: NavigationView = view.findViewById(R.id.toolbar)
+//        val bottomNavigationView: BottomNavigationView = view.findViewById(R.id.bottom_navigation)
+//
+//
+//
+//        //PAra el menu lateral emergente deslizando desde la izquierda debemos de:
+//
+//        //Debemos de inflar el menu lateral
+//         // Inflar el menú lateral
+//        val navController = findNavController()
+//
+//        var builderApp = AppBarConfiguration.Builder(navController.graph)
+//        builderApp.setOpenableLayout(drawerLayout)
+//      //  navView.setupWithNavController(navController)
+//
+//
+//
+//
+//        //FIXME: PANEL LATERAL- Referencia drawer layout
+//
+//        val appBarConfiguration = builderApp.build()
+//        setupActionBarWithNavController(navController = navController, appBarConfiguration)
+//
+//
+//        navView.setNavigationItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.Actividades -> {
+//                    findNavController().navigate(R.id.action_menuInicio_to_activitiesFragment)
+//                    // Acción para la opción 1
+//                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
+//                    return@setNavigationItemSelectedListener true
+//                }
+//
+//                R.id.Encontrar -> {
+//                    findNavController().navigate(R.id.action_menuInicio_to_mapaFragment)
+//
+//                    // Acción para la opción 2
+//                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
+//                    // replaceFragment(Fragment2())
+//                    return@setNavigationItemSelectedListener true
+//                }
+//
+//                R.id.AmigosRecientes -> {
+//                    findNavController().navigate(R.id.action_menuInicio_to_listaAmigosActivity)
+//                    // Acción para la opción 2
+//                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
+//                    // replaceFragment(Fragment2())
+//                    return@setNavigationItemSelectedListener true
+//
+//                }
+//                R.id.AniadirAmigos -> {
+//                    findNavController().navigate(R.id.action_menuInicio_to_agregarAmigosActivity)
+//                    return@setNavigationItemSelectedListener true
+//                }
+//
+//                else -> {
+//                    drawerLayout.closeDrawer(GravityCompat.START)
+//                    true
+//                }
+//            }
+//        }
+//
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.Ajustes -> {
+//                    // Acción para la opción 1 del BottomNavigationView
+//                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
+//                    // replaceFragment(Fragment1())
+//                    true
+//                }
+//
+//                R.id.info -> {
+//                    // Acción para la opción 2 del BottomNavigationView
+//                    // Puedes cargar un nuevo layout, iniciar una actividad, o realizar la acción que desees
+//                    //replaceFragment(Fragment2())
+//                    true
+//                }
+//
+//                else -> false
+//            }
+//        }
+//
+//        return view
+//    }
+//}
+//
