e99d5f74909535cf6e32d009d62c37feb2d9f3c9
a23davidcg <a23davidcg@iessanclemente.net>
2023-12-04 13:57:32 +0100

    revisar base datos para mapas e añadir navegacion a todos os botons e iconos

--- Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MapaFragment.kt
@@ -8,32 +8,39 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapaFragment : Fragment() {
 
     private lateinit var googleMap: GoogleMap
 
+    //PAra pillar la ubicacion desde un click
+    private var marcadorActual: Marker? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_mapa, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Initialize the fragment of the map
+        // inicializar el fragmento del mapa
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.mapaFragment) as SupportMapFragment
         mapFragment.getMapAsync { map ->
             // Configure the GoogleMap
             googleMap = map
             configurarMapa()
-            // You can perform additional map configurations here
+            //Configurar mapa para añadir por click
+
+            agregarListenerClicMapa()
+
         }
     }
 
@@ -43,9 +50,40 @@ class MapaFragment : Fragment() {
 
         // Mover la cámara a una ubicación específica (por ejemplo, latitud y longitud de un lugar)
         val ubicacion = LatLng(37.7749, -122.4194) // Latitud y longitud de San Francisco, CA
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 12f)) // Zoom de 12
+        val ubicacionSantiago =
+            LatLng(42.8805200, -8.5456900) //Ubicacion para santiago de compostela
+//        val ubicacionActual = LatLng()
+        googleMap.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                ubicacionSantiago,
+                12f
+            )
+        ) // Zoom de 12
 
         // Agregar un marcador en la ubicación
-        googleMap.addMarker(MarkerOptions().position(ubicacion).title("San Francisco"))
+        googleMap.addMarker(MarkerOptions().position(ubicacionSantiago).title("Santiago"))
     }
+
+    private fun agregarListenerClicMapa() {
+        googleMap.setOnMapClickListener { latLng ->
+            // Eliminar el marcador anterior si existe
+
+            marcadorActual?.remove()
+
+            // Agregar un nuevo marcador en la ubicación del clic
+            marcadorActual = googleMap.addMarker(
+                MarkerOptions().position(latLng).title("Ubicación personalizada")
+
+            )
+
+            // Puedes guardar la ubicación en una variable, base de datos, etc.
+            val latitud = latLng.latitude
+            val longitud = latLng.longitude
+
+
+
+            // Aquí puedes realizar acciones adicionales con la ubicación obtenida
+        }
+    }
+
 }
